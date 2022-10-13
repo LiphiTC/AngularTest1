11abf49f01b6f710aa57e82031262ec4ebab6170
LiphiTC <iprogcen@hotmail.com>
2022-10-13 18:47:54 +0300

    Fixed some typos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Filmaker';
+  
+  ngOnInit(): void {
+    Array.from(document.getElementsByClassName("hidden")).forEach(x => x.classList.remove("hidden"));
+  }
 }
